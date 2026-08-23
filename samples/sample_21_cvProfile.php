d1e13efefa427a1d4a170871a0a20f0829e3a3a7
Walter Dietz <134150136+WaltDietzney@users.noreply.github.com>
2026-08-23 19:12:41 +0200

    fix: make CV sample directly executable

--- samples/sample_21_cvProfile.php
+++ samples/sample_21_cvProfile.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
 use OdtTemplateEngine\Elements\ImageElement;
 use OdtTemplateEngine\Elements\ListElement;
 use OdtTemplateEngine\Elements\Paragraph;
