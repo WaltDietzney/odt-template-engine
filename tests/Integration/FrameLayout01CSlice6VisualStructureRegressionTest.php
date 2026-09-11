91b7aad01b044830f91c53511d7e8c222db8f682
Walter Dietz <134150136+WaltDietzney@users.noreply.github.com>
2026-09-11 21:00:11 +0200

    test: use fixture with matching image placeholder

--- tests/Integration/FrameLayout01CSlice6VisualStructureRegressionTest.php
+++ tests/Integration/FrameLayout01CSlice6VisualStructureRegressionTest.php
@@ -36,7 +36,7 @@ final class FrameLayout01CSlice6VisualStructureRegressionTest extends TestCase
     public function testParagraphAnchoredImageElementRemainsInsideParagraphCarrier(): void
     {
         $template = new OdtTemplate(
-            dirname(__DIR__, 2) . '/samples/templates/template_17_textfield.odt'
+            dirname(__DIR__, 2) . '/samples/templates/sample_textfeld.odt'
         );
         $this->templates[] = $template;
 
