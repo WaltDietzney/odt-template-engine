3c913c1cbd726fa29b35d3357ada857289d5a0ae
Walter Dietz <134150136+WaltDietzney@users.noreply.github.com>
2026-09-11 20:11:27 +0200

    feat: add structured insertion mode

---
+++ src/Document/StructuredInsertionMode.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OdtTemplateEngine\Document;
+
+/**
+ * Describes how a structured element participates in placeholder insertion.
+ */
+enum StructuredInsertionMode: string
+{
+    case BLOCK = 'block';
+    case INLINE_TEXT_FLOW = 'inline-text-flow';
+}
