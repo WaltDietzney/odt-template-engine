2783f238a72e6e268430f4f397428fdef8b3d2fa
Walter Dietz <134150136+WaltDietzney@users.noreply.github.com>
2026-09-11 11:14:54 +0200

    test: include TABLE-LAYOUT showcase in sample smoke

--- tests/Integration/PublicSampleSmokeTest.php
+++ tests/Integration/PublicSampleSmokeTest.php
@@ -38,7 +38,7 @@ final class PublicSampleSmokeTest extends TestCase
         $sampleFiles = $this->publicSampleFiles($this->temporaryDirectory . '/samples');
         $beforeOutput = $this->directorySnapshot($repositoryRoot . '/samples/output');
 
-        self::assertCount(25, $sampleFiles);
+        self::assertCount(26, $sampleFiles);
 
         foreach ($sampleFiles as $sampleFile) {
             $sampleName = pathinfo($sampleFile, PATHINFO_FILENAME);
@@ -89,7 +89,7 @@ final class PublicSampleSmokeTest extends TestCase
             }
 
             $sampleNumber = (int) $matches[1];
-            if ($sampleNumber >= 1 && $sampleNumber <= 25) {
+            if ($sampleNumber >= 1 && $sampleNumber <= 26) {
                 $sampleFiles[] = $sampleFile;
             }
         }
